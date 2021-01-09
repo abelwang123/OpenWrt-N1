45861886f3cb2c7a8749f656fa995de6356b263a
abelwang123 <68933125+abelwang123@users.noreply.github.com>
2021-01-09 10:35:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,6 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+#Add phicomm n1 install_emmc
+git clone https://github.com/tuanqing/install-program package/install-program
